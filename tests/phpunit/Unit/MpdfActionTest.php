1de90a1783778953d2507eb248fc52ef6ff217ea
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-06-28 14:50:27 +0200

    fix sniff Generic.WhiteSpace.DisallowSpaceIndent.SpacesUsed reported by phpcs

--- tests/phpunit/Unit/MpdfActionTest.php
+++ tests/phpunit/Unit/MpdfActionTest.php
@@ -7,81 +7,81 @@ use MediaWiki\MediaWikiServices;
  */
 class MpdfActionTest extends MediaWikiUnitTestCase {
 
-    protected $wgMpdfSimpleOutput;
+	protected $wgMpdfSimpleOutput;
 
-    public function testGetNameReturnsCorrectActionName() {
-        $action = $this->getMockBuilder( MpdfAction::class )
-                       ->disableOriginalConstructor()
-                       ->getMock();
+	public function testGetNameReturnsCorrectActionName() {
+		$action = $this->getMockBuilder( MpdfAction::class )
+					   ->disableOriginalConstructor()
+					   ->getMock();
 
-        // Define what getName() should return in the mocked MpdfAction
-        $action->expects( $this->any() )
-               ->method( 'getName' )
-               ->willReturn( 'mpdf' );
+		// Define what getName() should return in the mocked MpdfAction
+		$action->expects( $this->any() )
+			   ->method( 'getName' )
+			   ->willReturn( 'mpdf' );
 
-        // Call the getName() method
-        $name = $action->getName();
+		// Call the getName() method
+		$name = $action->getName();
 
-        // Assert that the returned name matches 'mpdf'
-        $this->assertEquals( 'mpdf', $name );
-    }
+		// Assert that the returned name matches 'mpdf'
+		$this->assertEquals( 'mpdf', $name );
+	}
 
-    public function testCheckOutputOfHtmlInsideShowMethod() {
-        // Set up expectations
-        $titleText = 'Test_Title';
-        $outputHtml = '<html><body>Test HTML</body></html>';
-        $format = 'html';
+	public function testCheckOutputOfHtmlInsideShowMethod() {
+		// Set up expectations
+		$titleText = 'Test_Title';
+		$outputHtml = '<html><body>Test HTML</body></html>';
+		$format = 'html';
 
-        // Mock objects
-        $outputPageMock = $this->createMock( OutputPage::class );
-        $titleMock = $this->createMock( Title::class );
-        $articleMock = $this->getMockBuilder( Article::class )
-                        ->disableOriginalConstructor()
-                        ->getMock();
+		// Mock objects
+		$outputPageMock = $this->createMock( OutputPage::class );
+		$titleMock = $this->createMock( Title::class );
+		$articleMock = $this->getMockBuilder( Article::class )
+						->disableOriginalConstructor()
+						->getMock();
 
-        $requestMock = $this->getMockBuilder( WebRequest::class )
-                       ->disableOriginalConstructor()
-                       ->getMock();
+		$requestMock = $this->getMockBuilder( WebRequest::class )
+					   ->disableOriginalConstructor()
+					   ->getMock();
 
-        $requestMock->expects( $this->any() )
-                ->method( 'getText' )
-                ->willReturn( 'format' );
+		$requestMock->expects( $this->any() )
+				->method( 'getText' )
+				->willReturn( 'format' );
 
-         // Mocking getTitle() method
-         $titleMock->expects( $this->any() )
-         ->method( 'getPrefixedText' )
-         ->willReturn( $titleText );
+		 // Mocking getTitle() method
+		 $titleMock->expects( $this->any() )
+		 ->method( 'getPrefixedText' )
+		 ->willReturn( $titleText );
 
-        // Mocking getOutput() method
-        $outputPageMock->expects( $this->any() )
-                    ->method( 'getOutput' )
-                    ->willReturn( $outputHtml );
+		// Mocking getOutput() method
+		$outputPageMock->expects( $this->any() )
+					->method( 'getOutput' )
+					->willReturn( $outputHtml );
 
-        // Mocking output() method
-        $outputPageMock->expects( $this->any() )
-                    ->method( 'output' )
-                    ->willReturn( $outputHtml );
-         
-        $titleToCheck = $titleMock->getPrefixedText();
-        $format = 'html';
-        $output = $outputPageMock;
+		// Mocking output() method
+		$outputPageMock->expects( $this->any() )
+					->method( 'output' )
+					->willReturn( $outputHtml );
+		 
+		$titleToCheck = $titleMock->getPrefixedText();
+		$format = 'html';
+		$output = $outputPageMock;
 
-        $filename = str_replace( [ '\\', '/', ':', '*', '?', '"', '<', '>', "\n", "\r", "\0" ], '_', $titleText );
-        MediaWikiServices::getInstance()->getHookContainer()->run( 'MpdfGetArticle', [ $titleText, &$article ] );
-        
-        $this->assertEquals( $titleText, $titleToCheck );
+		$filename = str_replace( [ '\\', '/', ':', '*', '?', '"', '<', '>', "\n", "\r", "\0" ], '_', $titleText );
+		MediaWikiServices::getInstance()->getHookContainer()->run( 'MpdfGetArticle', [ $titleText, &$article ] );
+		
+		$this->assertEquals( $titleText, $titleToCheck );
 
-        if ( !$this->wgMpdfSimpleOutput ) {
-            ob_start();
-            $articleMock->expects( $this->any() )
-                        ->method( 'view' )
-                        ->willReturn( $outputHtml );
+		if ( !$this->wgMpdfSimpleOutput ) {
+			ob_start();
+			$articleMock->expects( $this->any() )
+						->method( 'view' )
+						->willReturn( $outputHtml );
 
 			$html = $articleMock->view();
 			$outputPageMock->output();
 			
-            $this->assertStringContainsString( "Test HTML", $html );
-            $html = ob_get_clean();
-        }
-    }
+			$this->assertStringContainsString( "Test HTML", $html );
+			$html = ob_get_clean();
+		}
+	}
 }
