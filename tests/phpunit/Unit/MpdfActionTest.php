d6970c7451a6102852c6ade3dc44295b8ea2e7c7
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-06-28 14:40:46 +0200

    fix sniff MediaWiki.WhiteSpace.SpaceyParenthesis reported by phpcs

--- tests/phpunit/Unit/MpdfActionTest.php
+++ tests/phpunit/Unit/MpdfActionTest.php
@@ -10,20 +10,20 @@ class MpdfActionTest extends MediaWikiUnitTestCase {
     protected $wgMpdfSimpleOutput;
 
     public function testGetNameReturnsCorrectActionName() {
-        $action = $this->getMockBuilder(MpdfAction::class)
+        $action = $this->getMockBuilder( MpdfAction::class )
                        ->disableOriginalConstructor()
                        ->getMock();
 
         // Define what getName() should return in the mocked MpdfAction
-        $action->expects($this->any())
-               ->method('getName')
-               ->willReturn('mpdf');
+        $action->expects( $this->any() )
+               ->method( 'getName' )
+               ->willReturn( 'mpdf' );
 
         // Call the getName() method
         $name = $action->getName();
 
         // Assert that the returned name matches 'mpdf'
-        $this->assertEquals('mpdf', $name);
+        $this->assertEquals( 'mpdf', $name );
     }
 
     public function testCheckOutputOfHtmlInsideShowMethod() {
@@ -33,34 +33,34 @@ class MpdfActionTest extends MediaWikiUnitTestCase {
         $format = 'html';
 
         // Mock objects
-        $outputPageMock = $this->createMock(OutputPage::class);
-        $titleMock = $this->createMock(Title::class);
-        $articleMock = $this->getMockBuilder(Article::class)
+        $outputPageMock = $this->createMock( OutputPage::class );
+        $titleMock = $this->createMock( Title::class );
+        $articleMock = $this->getMockBuilder( Article::class )
                         ->disableOriginalConstructor()
                         ->getMock();
 
-        $requestMock = $this->getMockBuilder(WebRequest::class)
+        $requestMock = $this->getMockBuilder( WebRequest::class )
                        ->disableOriginalConstructor()
                        ->getMock();
 
-        $requestMock->expects($this->any())
-                ->method('getText')
-                ->willReturn('format');
+        $requestMock->expects( $this->any() )
+                ->method( 'getText' )
+                ->willReturn( 'format' );
 
          // Mocking getTitle() method
-         $titleMock->expects($this->any())
-         ->method('getPrefixedText')
-         ->willReturn($titleText);
+         $titleMock->expects( $this->any() )
+         ->method( 'getPrefixedText' )
+         ->willReturn( $titleText );
 
         // Mocking getOutput() method
-        $outputPageMock->expects($this->any())
-                    ->method('getOutput')
-                    ->willReturn($outputHtml);
+        $outputPageMock->expects( $this->any() )
+                    ->method( 'getOutput' )
+                    ->willReturn( $outputHtml );
 
         // Mocking output() method
-        $outputPageMock->expects($this->any())
-                    ->method('output')
-                    ->willReturn($outputHtml);
+        $outputPageMock->expects( $this->any() )
+                    ->method( 'output' )
+                    ->willReturn( $outputHtml );
          
         $titleToCheck = $titleMock->getPrefixedText();
         $format = 'html';
@@ -69,18 +69,18 @@ class MpdfActionTest extends MediaWikiUnitTestCase {
         $filename = str_replace( [ '\\', '/', ':', '*', '?', '"', '<', '>', "\n", "\r", "\0" ], '_', $titleText );
         MediaWikiServices::getInstance()->getHookContainer()->run( 'MpdfGetArticle', [ $titleText, &$article ] );
         
-        $this->assertEquals($titleText, $titleToCheck);
+        $this->assertEquals( $titleText, $titleToCheck );
 
         if ( !$this->wgMpdfSimpleOutput ) {
             ob_start();
-            $articleMock->expects($this->any())
-                        ->method('view')
-                        ->willReturn($outputHtml);
+            $articleMock->expects( $this->any() )
+                        ->method( 'view' )
+                        ->willReturn( $outputHtml );
 
 			$html = $articleMock->view();
 			$outputPageMock->output();
 			
-            $this->assertStringContainsString( "Test HTML", $html);
+            $this->assertStringContainsString( "Test HTML", $html );
             $html = ob_get_clean();
         }
     }
