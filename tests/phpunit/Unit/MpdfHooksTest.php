a52dc1a56af2e503869d16aa4771b74b418ac3c5
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-06-29 11:24:00 +0200

    add more tests, update ci.yml
    - add unit tests;
    - update ci.yml for test coverage

---
+++ tests/phpunit/Unit/MpdfHooksTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * @group MpdfHooks
+ */
+class MpdfHooksTest extends MediaWikiUnitTestCase {
+
+	protected function setUp(): void {
+		parent::setUp();
+		global $wgMpdfToolboxLink;
+		$wgMpdfToolboxLink = true;
+	}
+
+	/**
+	 * @covers MpdfHooks::onSidebarBeforeOutput
+	 */
+	public function testOnSidebarBeforeOutputWhenMpdfToolboxLinkIsFalse() {
+		global $wgMpdfToolboxLink;
+		$wgMpdfToolboxLink = false;
+
+		$skin = $this->createMock( Skin::class );
+		$sidebar = [];
+
+		$result = MpdfHooks::onSidebarBeforeOutput( $skin, $sidebar );
+
+		$this->assertTrue( $result );
+		$this->assertArrayNotHasKey( 'mpdf', $sidebar[ 'TOOLBOX' ] ?? [] );
+	}
+
+	/**
+	 * @covers MpdfHooks::onSidebarBeforeOutput
+	 */
+	public function testOnSidebarBeforeOutputWhenSpecialPage() {
+		global $wgMpdfToolboxLink;
+		$wgMpdfToolboxLink = true;
+
+		$title = $this->createMock( Title::class );
+		$title->method( 'isSpecialPage' )->willReturn( true );
+
+		$skin = $this->createMock( Skin::class );
+		$skin->method( 'getTitle' )->willReturn( $title );
+
+		$sidebar = [];
+
+		$result = MpdfHooks::onSidebarBeforeOutput( $skin, $sidebar );
+
+		$this->assertTrue( $result );
+		$this->assertArrayNotHasKey( 'mpdf', $sidebar[ 'TOOLBOX' ] ?? [] );
+	}
+
+	/**
+	 * @covers MpdfHooks::onSidebarBeforeOutput
+	 */
+	public function testOnSidebarBeforeOutputAddsMpdfLink() {
+		global $wgMpdfToolboxLink;
+		$wgMpdfToolboxLink = true;
+
+		$title = $this->createMock( Title::class );
+		$title->method( 'isSpecialPage' )->willReturn( false );
+		$title->method( 'getLocalUrl' )->with( [ 'action' => 'mpdf' ] )->willReturn( '/index.php?action=mpdf' );
+
+		$skin = $this->createMock( Skin::class );
+		$skin->method( 'getTitle' )->willReturn( $title );
+
+		$sidebar = [];
+
+		$result = MpdfHooks::onSidebarBeforeOutput( $skin, $sidebar );
+
+		$this->assertTrue( $result );
+		$this->assertArrayHasKey( 'TOOLBOX', $sidebar );
+		$this->assertArrayHasKey( 'mpdf', $sidebar[ 'TOOLBOX' ] );
+		$this->assertEquals( 'mpdf-action', $sidebar[ 'TOOLBOX' ][ 'mpdf' ][ 'msg' ] );
+		$this->assertEquals( '/index.php?action=mpdf', $sidebar[ 'TOOLBOX' ][ 'mpdf' ][ 'href' ] );
+		$this->assertEquals( 't-mpdf', $sidebar[ 'TOOLBOX' ][ 'mpdf' ][ 'id' ] );
+		$this->assertEquals( 'mpdf', $sidebar[ 'TOOLBOX' ][ 'mpdf' ][ 'rel' ] );
+	}
+}
